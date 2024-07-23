bab832511682341992b3b8f48a8bca85e1a7c0cd
Satees <80681460+Satish9047@users.noreply.github.com>
2024-07-23 15:35:28 +0545

    Update expertise.svelte

--- src/components/expertise.svelte
+++ src/components/expertise.svelte
@@ -68,9 +68,9 @@
 <main class=" flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 sm:gap-1 sm:w-5/6 ">
     
     {#each Technologies as technology}
-    <div class=" md:ml-5 m-1 px-4 py-1 flex justify-start items-center w-screen sm:w-5/6 bg-gray-300 hover:bg-orange-300 overflow-y-scroll rounded-md">
+    <div class=" md:ml-5 m-1 px-4 py-1 flex justify-start items-center w-screen sm:w-5/6 bg-gray-300 hover:bg-orange-300 overflow-y-scroll rounded-md no-scroll">
         <img src={technology.icon} alt={technology.name} width={100} height={100} class="w-10 sm:w-14 md:w-16"/>
-        <div class=" m-1 px-2 flex flex-col justify-start">
+        <div class=" m-1 px-2 flex flex-col justify-start no-scroll">
             <h2>{technology.name}</h2>
             <p>Level: {technology.level}</p>
         </div>
